298bb630e0a43b44f8f83a1cc2a0c63dd7756342
Kash and Garvi <masterrgame223@gmail.com>
2025-01-13 13:26:35 -0800

    fixed something useless

--- updatore.bat
+++ updatore.bat
@@ -1,6 +1,6 @@
 @echo off
 set str=hey man are you ready to update to the lasterst version?
-set str2=ok i think it updated check the text above 2 make sure <3
+set strrr=ok i think it updated check the text above 2 make sure <3
 echo %str% 
 pause
 
@@ -9,5 +9,5 @@ git branch -M main
 git fetch
 git pull origin main
 
-echo %str2% 
+echo %strrr%
 pause
